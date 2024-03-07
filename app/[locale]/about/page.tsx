b5ae5948324c3d33713d46c08f293ff6ecabc2de
yuanqiang.luo <yuanqiang.luo>
2024-03-07 20:06:40 +0800

    提交

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -15,8 +15,8 @@ export default async function Page({ params: { locale } }: AboutProps) {
       <p>{t('about3')}</p>
 
       <p className="pt-10">{t('about4')}</p>
-      <p>{t('about5')}</p>
-      <p>{t('about6')}</p>
+      <p>{t('about5')} <span><a  href='https://x.com/loreluo2077/status/1765709743346040905?s=46&t=ZOKaD0FE_oyDoR3SZX7tTA'>here</a></span></p>
+      {/* <p>{t('about6')}</p> */}
 
       <div className="container prose mx-auto"></div>
       <footer className="footer footer-center rounded bg-base-200  text-base-content">
