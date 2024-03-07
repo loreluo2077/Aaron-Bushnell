6c4054c53ffe403884e8564fbabcbc096e2397dd
yuanqiang.luo <yuanqiang.luo>
2024-03-07 20:47:31 +0800

    修复一点问题

--- app/api/light/route.js
+++ app/api/light/route.js
@@ -1,6 +1,6 @@
 export async function GET() {
+  const time= Date.parse(new Date().toString())
 
-
-  return fetch('http://www.loreluo.com/light/increaseLightNum',{method:'GET'})
-
+  return fetch('http://www.loreluo.com/light/increaseLightNum?time='+time,{method:'GET'})
+  
 }
